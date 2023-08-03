fd50449978ca4e0f6d4e80b9043384b14f714cc1
Alexey Kotcov <richyshark@gmail.com>
2023-08-03 23:45:38 +0300

    no redir pls

--- src/app/Components/Form.tsx
+++ src/app/Components/Form.tsx
@@ -72,8 +72,8 @@ export default function OrderForm() {
         <h1 className="text-white">FormSubmit Demo</h1>
         <form target="_blank" action="https://formsubmit.co/richyshark@gmail.com" method="POST" className="mt-4">
             <div className="px-6 flex flex-col space-y-4">
-                <input type="hidden" name="_next" value="https://autoprikur.vercel.app/"/>
-                <input type="hidden" name="_captcha" value="false"/>
+                <input type="hidden" name="_next" value="https://autoprikur.vercel.app/#uslugi"/>
+                {/* <input type="hidden" name="_captcha" value="false"/> */}
                 <input type="email" name="email" className="" placeholder="Email Address" required/>
                 <input type="text" name="name" className="" placeholder="Full Name" required/>
                 <input placeholder="Your Message" className="" name="message" required></input>
