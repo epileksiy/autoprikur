7a37f728f4b247268d307b34f5d899719bba72ad
Alexey Kotcov <richyshark@gmail.com>
2023-08-04 16:42:53 +0300

    order form

--- src/app/Components/Form.tsx
+++ src/app/Components/Form.tsx
@@ -68,17 +68,19 @@ export default function OrderForm() {
     //   </form>
 
 
-    <div className="mt-10 p-5 bg-zinc-800 flex flex-col justify-center text-center">
-        <h1 className="text-white">FormSubmit Demo</h1>
+    <div className="mt-10 px-5 py-8 bg-zinc-800 flex flex-col justify-center text-center w-full md:w-1/3 rounded-2xl">
+        <p className="text-white text-2xl">Заполните заявку онлайн</p>
+        <p className="text-white text-lg mt-3">Наша команда обработает заявку и сразу <span className="text-red-500">Вам</span> перезвонит</p>
         <form target="_blank" action="https://formsubmit.co/richyshark@gmail.com" method="POST" className="mt-4">
             <div className="px-6 flex flex-col space-y-4">
                 <input type="hidden" name="_next" value="https://autoprikur.vercel.app/#uslugi"/>
                 <input type="hidden" name="_captcha" value="false"/>
-                <input type="email" name="email" className="" placeholder="Email Address" required/>
-                <input type="text" name="name" className="" placeholder="Full Name" required/>
-                <input placeholder="Your Message" className="" name="message" required></input>
+                <input type="text" name="name" className="h-12 p-4 rounded-lg" placeholder="Ваше имя" required/>
+                <input type="text" name="car" className="h-12 p-4 rounded-lg" placeholder="Марка автомобиля" required/>
+                <input type="text" name="phone" className="h-12 p-4 rounded-lg" placeholder="Номер телефона" required/>
+                <input placeholder="Опишите Вашу поломку.." className=" h-52 p-4 rounded-lg" name="problem" required></input>
             </div>
-            <button type="submit" className="btn btn-lg btn-dark btn-block text-white bg-red-600 p-3 rounded-lg mt-6">Submit Form</button>
+            <button type="submit" className="btn btn-lg btn-dark btn-block text-white bg-red-600 p-3 rounded-lg mt-12">Оставить заявку</button>
         </form>
     </div>
     )
